28522c6fc52991065405270df16440a68e980530
parnasos <lauragraizer@gmail.com>
2021-06-16 19:30:57 +0200

    Add simbols to status

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -3,6 +3,15 @@ import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import "../stylesheets/characterDetail.scss";
 
 const CharacterDetails = (props) => {
+  const makeDead = () => {
+    if (props.user.status === "Dead") {
+      return "fas fa-dizzy";
+    } else if (props.user.status === "Alive") {
+      return "fas fa-smile-wink";
+    } else {
+      return "fas fa-question-circle";
+    }
+  };
   return (
     <div className="character__container">
       <article className="character__detail">
@@ -19,7 +28,10 @@ const CharacterDetails = (props) => {
             <li className="card__details--item">
               Species: {props.user.species}
             </li>
-            <li className="card__details--item">Status: {props.user.status}</li>
+            <li className="card__details--item ">
+              Status: {props.user.status}
+              <i class={`${makeDead()}`}></i>
+            </li>
             <li className="card__details--item">Origin: {props.user.origin}</li>
             <li className="card__details--item">
               Number of episodes: {props.user.episodes}
